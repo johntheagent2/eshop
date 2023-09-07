aada0a1c088f44a8bb6b9f232045b121440fae2f
johntheagent2 <caophat113@gmail.com>
2023-09-07 11:27:50 +0700

    fix controller not working

--- src/main/java/cdp/mycomp/eshop/controller/CategoryController.java
+++ src/main/java/cdp/mycomp/eshop/controller/CategoryController.java
@@ -3,21 +3,20 @@ package cdp.mycomp.eshop.controller;
 import cdp.mycomp.eshop.model.Category;
 import cdp.mycomp.eshop.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController("/api/v1/category")
+@RestController
+@RequestMapping("/category")
 public class CategoryController {
     @Autowired
     private CategoryService categoryService;
 
     @GetMapping()
     public List<Category> getAllCategory(){
-        return categoryService.getAll();
+        List<Category> categoryList = categoryService.getAll();
+        return categoryList.isEmpty() ? null : categoryList;
     }
 
     @PostMapping("/save")
